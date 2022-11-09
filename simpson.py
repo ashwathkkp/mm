435197a51a9b3c0a07d258ed05eaae6e9d624c8d
Krishnan S G <43802499+KrishnanSG@users.noreply.github.com>
2022-11-09 09:54:42 +0530

    Update simpson.py

--- simpson.py
+++ simpson.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-a = 0
-b = np.pi
-n = 11
-h = (b - a) / (n - 1)
+a = 0   # lower limit of integral
+b = np.pi # upper limit of integral
+n = 11 # no. of intervals
+h = (b - a) / (n - 1) 
 x = np.linspace(a, b, n)
-f = np.sin(x)
+f = np.sin(x) # function to integrate
 
 I_simp = (h/3) * (f[0] + 2*sum(f[:n-2:2]) \
             + 4*sum(f[1:n-1:2]) + f[n-1])
